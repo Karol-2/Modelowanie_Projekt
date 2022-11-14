5b276d73647958e9ea4cabbf059775b2668f9d10
Karol-2 <97386279+Karol-2@users.noreply.github.com>
2022-11-14 23:20:39 +0100

    POTEZNE ZROBIONE ZADANIE

--- Spółki/spolka_CDR.R
+++ Spółki/spolka_CDR.R
@@ -10,29 +10,60 @@ library(fitdistrplus)
 
 class(dane)
 
-cena_zamkniecia <- dane$Zamkniecie
+kurs_zamkniecia <- dane$Zamkniecie
 
-par(mfrow = c(3,1))
+par(mfrow = c(1,1))
+
 # 1
-plot(cena_zamkniecia)
+plot(kurs_zamkniecia)
 
-hist(cena_zamkniecia, prob=TRUE)
+hist(kurs_zamkniecia, prob=TRUE)
+
 # 2
-mean(cena_zamkniecia)
-sd(cena_zamkniecia)
-skewness(cena_zamkniecia)
-kurtosis(cena_zamkniecia)
+mean(kurs_zamkniecia)
+sd(kurs_zamkniecia)
+skewness(kurs_zamkniecia)
+kurtosis(kurs_zamkniecia)
+
 # 3
 plot.legend <- c('norm','lognorm','gamma')
 
-normalny <- fitdist(cena_zamkniecia,"norm")
-normalny
+estymator_normalny <- fitdist(kurs_zamkniecia,"norm")
+estymator_normalny
 
-lognorm <- fitdist(cena_zamkniecia,"lnorm")
-lognorm
+estymator_lognorm <- fitdist(kurs_zamkniecia,"lnorm")
+estymator_lognorm
 
-gamma <- fitdist(cena_zamkniecia,"gamma")
-gamma
+estymator_gamma <- fitdist(kurs_zamkniecia,"gamma")
+estymator_gamma
 
-denscomp(list(normalny,lognorm,gamma), legendtext = plot.legend)
+# 4
+denscomp(list(estymator_normalny,estymator_lognorm,estymator_gamma), legendtext = plot.legend)
+cdfcomp(list(estymator_normalny,estymator_lognorm,estymator_gamma), legend = plot.legend)
+qqcomp(list(estymator_normalny,estymator_lognorm,estymator_gamma), legend = plot.legend)
+
+#kryteria wyboru - wychodzi że LOGNORM
+gofstat(list(estymator_normalny, estymator_lognorm, estymator_gamma),  fitnames = plot.legend)
+
+# 5
+N <- 10000
+n <- 100
+
+Dn <- c()
+
+for (i in 1:N) {
+  Yln <- rlnorm(n, estymator_lognorm$estimate[1], estymator_lognorm$estimate[2])
+  
+  Dn[i] <- ks.test(Yln, plnorm, estymator_lognorm$estimate[1],estymator_lognorm$estimate[2], exact = TRUE)$statistic
+}
+
+dn <- ks.test(kurs_zamkniecia, plnorm, estymator_lognorm$estimate[1], estymator_lognorm$estimate[2], exact = TRUE)$statistic
+
+hist(Dn, prob = T)
+points(dn, 0, pch = 19, col = 2)
+
+p_value <- length(Dn[Dn>dn])/N; p_value
+alpha <- 0.05
+p_value <- alpha
+# Odrzucamy test zgodnosci ?
 
