8305ab470f18e5ee4aeb49859d58cb95445bc9e1
Piterson25 <piotrmaszczak25@gmail.com>
2022-11-15 17:52:24 +0100

    Zadania 1-5 11bit

--- Spółki/spolka_11bit.R
+++ Spółki/spolka_11bit.R
@@ -1,5 +1,4 @@
-#dane <- read.csv("//NAS1/home/pmaszczak/Desktop/modelowanie/a11bit.csv")
-dane <- read.csv("D:/Studia/3sem/Modelowanie matematyczne/Modelowanie_Projekt_1/Spółki/11bit.csv")
+dane <- read.csv("//NAS1/home/pmaszczak/Desktop/Modelowanie_Projekt_1/Spółki/11bit.csv")
 library(moments)
 library(ggplot2)
 library(fitdistrplus)
@@ -12,32 +11,25 @@ cena_zamkniecia <- dane$Zamknięcie
 #1
 par(mfrow = c(1, 1))
 
-plot(cena_zamkniecia)
+plot(cena_zamkniecia, main="Wykres kursow zamkniecia akcji 11bit", xlab="Notowany dzien", ylab="Cena zamkniecia")
 
-hist(cena_zamkniecia, main="Histogram", xlab="cena zamkniecia", ylab="gestosc", prob=TRUE)
+hist(cena_zamkniecia, main="Histogram", xlab="Cena zamkniecia", ylab="Gestosc", prob=TRUE)
 
 #2
-srednia <- mean(cena_zamkniecia)
-srednia
+srednia <- mean(cena_zamkniecia); srednia
 
-odchylenie_st <- sd(cena_zamkniecia)
-odchylenie_st
+odchylenie_st <- sd(cena_zamkniecia); odchylenie_st
 
-skosnosc <- skewness(cena_zamkniecia)
-skosnosc
+skosnosc <- skewness(cena_zamkniecia); skosnosc
 
-kurtoza <- kurtosis(cena_zamkniecia)
-skosnosc
+kurtoza <- kurtosis(cena_zamkniecia); kurtoza
 
 #3
-norm <- fitdist(cena_zamkniecia, "norm")
-norm
+norm <- fitdist(cena_zamkniecia, "norm"); norm
 
-lnorm <- fitdist(cena_zamkniecia, "lnorm")
-lnorm
+lnorm <- fitdist(cena_zamkniecia, "lnorm"); lnorm
 
-gamma <- fitdist(cena_zamkniecia, "gamma")
-gamma
+gamma <- fitdist(cena_zamkniecia, "gamma"); gamma
 
 #4
 plot.legend <- c('log-norm', 'norm', 'gamma')
@@ -46,31 +38,37 @@ fn <- fitdist(cena_zamkniecia, "norm")
 fln <- fitdist(cena_zamkniecia, "lnorm")
 fg <- fitdist(cena_zamkniecia, "gamma")
 
-denscomp(list(fn, fln, fg), legendtext = plot.legend)
-cdfcomp(list(fn, fln, fg), legendtext = plot.legend)
-qqcomp(list(fn, fln, fg), legendtext = plot.legend)
-ppcomp(list(fn, fln, fg), legendtext = plot.legend)
+funkcje <- list(fn, fln, fg)
+legenda = c("normalny","log-normalny","gamma")
 
-gofstat(list(fn, fln, fg),  fitnames = plot.legend)
+cdfcomp(funkcje, legend = legenda)
+denscomp(funkcje, legend = legenda)
+qqcomp(funkcje, legend = legenda)
+ppcomp(funkcje, legend = legenda)
+gofstat(funkcje, fitnames = legenda)
+# Ceny zamkniecia najlepiej opisuje rozklad log-normalny
 
 # 5
 
 N <- 10000
-n <- 100
+n <- length(cena_zamkniecia)
 
-Dn <- c()
+Dln <- c()
 
 for (i in 1:N) {
   Yln <- rlnorm(n, fln$estimate[1], fln$estimate[2])
   
-  Dn[i] <- ks.test(Yln, plnorm, fln$estimate[1],fln$estimate[2], exact = TRUE)$statistic
+  Dln[i] <- ks.test(Yln, plnorm, fln$estimate[1],fln$estimate[2], exact = TRUE)$statistic
 }
 
-dn <- ks.test(cena_zamkniecia, plnorm, fln$estimate[1], fln$estimate[2], exact = TRUE)$statistic
+dn_ln <- ks.test(cena_zamkniecia, plnorm, fln$estimate[1], fln$estimate[2], exact = TRUE)$statistic
+dn_ln
+# Wartosc statystyki Dln - dn_ln
 
-hist(Dn, prob = T)
-points(dn, 0, pch = 19, col = 2)
+hist(Dln, prob=T, xlim=c(0, 0.15), ylim=c(0, 40))
+points(dn_ln, 0, pch = 19, col = 2)
 
-p_value <- length(Dn[Dn>dn])/N; p_value
+p_value_ln <- length(Dln[Dln>dn_ln])/N; p_value_ln
 alpha <- 0.05
-# JEST GIT, POTWIERDAZAM
+p_value_ln <= alpha
+# Hipoteza odrzucona
