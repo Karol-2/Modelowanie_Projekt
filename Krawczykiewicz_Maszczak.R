1de13f43471694f68695f1d5ac6f96d9648e95cc
Piterson25 <piotrmaszczak25@gmail.com>
2023-01-27 19:39:38 +0100

    Poprawki w 2 części projektu

--- Krawczykiewicz_Maszczak.R
+++ Krawczykiewicz_Maszczak.R
@@ -1,5 +1,5 @@
-cdr <- read.csv("D:/Studia/3sem/Modelowanie matematyczne/Modelowanie_Projekt_1/Spółki/cdr.csv")
-bit11 <- read.csv("D:/Studia/3sem/Modelowanie matematyczne/Modelowanie_Projekt_1/Spółki/11bit.csv")
+cdr <- read.csv("C:/Studia/Modelowanie matematyczne/Modelowanie_Projekt_1/Spółki/cdr.csv")
+bit11 <- read.csv("C:/Studia/Modelowanie matematyczne/Modelowanie_Projekt_1/Spółki/11bit.csv")
 
 library(ggplot2)
 library(moments)
